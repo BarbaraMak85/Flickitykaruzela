686d79bd62805183101ad98a22c63522bcdf973b
Barbara Makowiec <barbara.makowiec@wp.pl>
2019-01-22 11:00:17 +0100

    change mustache

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+var caurselImages = [{
+        image: "Kraina pewnego czarnoksieznika.jpg",
+        title: '\"Kraina pewnego czarnoksiężnika\"',
+        description: "Tomasz Sętowski",
+        id: 1
+    },
+    {
+        image: "Cwiczenia z lewitacji.jpg",
+        title: '\"Ćwiczenia z lewitacji\"',
+        description: "Tomasz Sętowski",
+        id: 2
+    },
+    {
+        image: "Ikona czasu.jpg",
+        title: '\"Ikona czasu\"',
+        description: "Tomasz Sętowski",
+        id: 3
+    },
+    {
+        image: "Dreamand.jpg",
+        title: '\"Dreamand\"',
+        description: "Tomasz Sętowski",
+        id: 4
+    },
+    {
+        image: "Kosci zostaly rzucone.jpg",
+        title: '\"Kosci zostaly rzucone\"',
+        description: "Tomasz Sętowski",
+        id: 5
+    }
+];
+
+var templateList = document.getElementById('template-product-item').innerHTML;
+Mustache.parse(templateList);
+
+var results = document.getElementById('carousel');
+
+caurselImages.forEach(x => {
+    var fullProductList = Mustache.render(templateList, x);
+    results.insertAdjacentHTML('beforeend', fullProductList);
+});
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
     // options
